d6ce1cbe10d488455deaa9a63c78e540ffa00953
Chuck Che <cche@sumologic.com>
2016-06-29 17:32:11 -0700

    Revert "Add refresh interval index setting and support same key multi values in completion context"

--- elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/CompletionDsl.scala
+++ elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/CompletionDsl.scala
@@ -18,12 +18,9 @@
  */
 package com.sumologic.elasticsearch.restlastic.dsl
 
-import org.json4s.JsonAST.JObject
-import org.json4s.JsonDSL._
-
 trait CompletionDsl extends DslCommons with QueryDsl {
 
-  case class Suggest(text: String, completion: CompletionWithContext) extends Query with RootObject {
+  case class Suggest(text: String, completion: Completion) extends Query with RootObject {
     val _suggest = "suggest"
     val _text = "text"
     val _completion = "completion"
@@ -36,40 +33,20 @@ trait CompletionDsl extends DslCommons with QueryDsl {
     }
   }
 
-  trait CompletionWithContext {
+  case class Completion(field: String, size: Int, context: Map[String, String]) extends Query {
     val _field = "field"
     val _context = "context"
     val _size = "size"
 
-    val field: String
-    val size: Int
-    def context: Iterable[(String, String)]
+    def withAdditionalContext(newContext: (String, String)*) = {
+      this.copy(context = context ++ newContext)
+    }
 
-    def toJson: Map[String, Any] = Map(
+    override def toJson: Map[String, Any] = Map(
       _field -> field,
-      _context -> context.foldLeft(JObject()) {(k, v) => k ~ v },
+      _context -> context,
       _size -> size
     )
-  }
-
-  case class Completion(field: String, size: Int, context: Map[String, String])
-    extends Query with CompletionWithContext {
-
-    def withAdditionalContext(newContext: (String, String)*) = {
-      this.copy(context = context ++ newContext)
-    }
-  }
-
-  /**
-   * Support context filters with same key and different values
-   * e.g. context: key->val1, key->val2
-   */
-  case class CompletionWithSeqContext(field: String, size: Int, context: Seq[(String, String)])
-    extends Query with CompletionWithContext {
-
-    def withAdditionalContext(newContext: (String, String)*) = {
-      this.copy(context = context ++ newContext)
-    }
   }
 }
 
--- elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/IndexDsl.scala
+++ elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/IndexDsl.scala
@@ -64,17 +64,15 @@ trait IndexDsl extends DslCommons {
     }
   }
 
-  case class IndexSetting(numberOfShards: Int, numberOfReplicas: Int, analyzer: Analyzer, refreshInterval: Int = 1) extends EsOperation {
+  case class IndexSetting(numberOfShards: Int, numberOfReplicas: Int, analyzer: Analyzer) extends EsOperation {
     val _shards = "number_of_shards"
     val _replicas = "number_of_replicas"
     val _analysis = "analysis"
-    val _refreshInterval = "refresh_interval"
 
     override def toJson: Map[String, Any] = Map(
       _shards -> numberOfShards,
       _replicas -> numberOfReplicas,
-      _analysis -> analyzer.toJson,
-      _refreshInterval -> refreshInterval)
+      _analysis -> analyzer.toJson)
   }
 
   case class Analyzer(name: Name, tokenizer: FieldType, filter: FieldType) extends EsOperation {
