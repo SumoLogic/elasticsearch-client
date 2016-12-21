9d338213adf7c5ef2fc500912e8d518bbf078924
Clay McCoy <github@claymccoy.com>
2016-12-20 23:13:22 -0600

    Update elastic search dependency. Remove direct lucene dependency for compatibility. Minor changes to integration test client needed to compile now.

--- elasticsearch-test/src/main/scala/com/sumologic/elasticsearch_test/ElasticsearchIntegrationTest.scala
+++ elasticsearch-test/src/main/scala/com/sumologic/elasticsearch_test/ElasticsearchIntegrationTest.scala
@@ -22,7 +22,7 @@ import java.io.File
 
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
 import org.elasticsearch.client.transport.TransportClient
-import org.elasticsearch.common.settings.ImmutableSettings
+import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.{InetSocketTransportAddress, LocalTransportAddress}
 import org.elasticsearch.node.NodeBuilder
 import org.scalatest.{BeforeAndAfterAll, Suite}
@@ -71,10 +71,14 @@ trait ElasticsearchIntegrationTest extends BeforeAndAfterAll {
 
 object ElasticsearchIntegrationTest {
   private val r = new Random()
-  private lazy val esNodeSettings = ImmutableSettings.settingsBuilder().put("path.data", createTempDir("elasticsearch-test")).build()
+  private val workingDir = createTempDir("elasticsearch-test")
+  private lazy val esNodeSettings = Settings.settingsBuilder()
+    .put("path.data", workingDir)
+    .put("path.home", workingDir)
+    .build()
   private lazy val esNode = NodeBuilder.nodeBuilder().local(true).settings(esNodeSettings).node()
-  private lazy val settings = ImmutableSettings.settingsBuilder().put("node.local", "true").build()
-  lazy val client = new TransportClient(settings).addTransportAddress(new LocalTransportAddress("1"))
+  private lazy val settings = Settings.settingsBuilder().put("node.local", "true").build()
+  lazy val client = new TransportClient.Builder().settings(settings).build().addTransportAddress(new LocalTransportAddress("1"))
   lazy val globalEndpoint = {
     val nodeInfos = client.admin().cluster().prepareNodesInfo().clear().setSettings(true).setHttp(true).get()
     val nodeAddress =
