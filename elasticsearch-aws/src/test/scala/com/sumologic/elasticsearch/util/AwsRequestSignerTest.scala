275b8a6a1c9544ca715058ce5eb026e0c1e17a67
Sean Quigley <seanq@giphy.com>
2017-04-17 06:24:33 -0400

    use new Akka version

--- elasticsearch-aws/src/test/scala/com/sumologic/elasticsearch/util/AwsRequestSignerTest.scala
+++ elasticsearch-aws/src/test/scala/com/sumologic/elasticsearch/util/AwsRequestSignerTest.scala
@@ -25,9 +25,6 @@ import com.amazonaws.auth.{AWSCredentials, AWSSessionCredentials}
 import com.sumologic.elasticsearch.restlastic.dsl.Dsl._
 import org.apache.http.HttpHeaders
 import org.scalatest.{Matchers, WordSpec}
-//import spray.http.HttpHeaders.{Host, RawHeader}
-//import spray.http.Uri.Query
-//import spray.http.{HttpEntity, _}
 
 class AwsRequestSignerTest extends WordSpec with Matchers {
   val dummyCredentials = new AWSCredentials {
