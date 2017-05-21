630eb316c67e73ce80e45a4a5ee554f177829395
Chuck Che <cche@sumologic.com>
2017-05-20 22:48:36 -0700

    5.1 only support PUT for creating index

--- elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/RestlasticSearchClient.scala
+++ elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/RestlasticSearchClient.scala
@@ -159,7 +159,7 @@ class RestlasticSearchClient(endpointProvider: EndpointProvider, signer: Option[
 
   def createIndex(index: Index, settings: Option[IndexSetting] = None): Future[RawJsonResponse] = {
     implicit val ec = indexExecutionCtx
-    runEsCommand(CreateIndex(settings), s"/${index.name}").recover {
+    runEsCommand(CreateIndex(settings), s"/${index.name}", PUT).recover {
       case ElasticErrorResponse(message, status) if message contains "IndexAlreadyExistsException" =>
         throw new IndexAlreadyExistsException(message)
     }
