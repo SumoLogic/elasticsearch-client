232cf8fab899c848775f08378ed2019a78216499
Ankur Khurana <ankur@sumologic.com>
2018-01-24 00:34:29 +0530

    Issue-153: Fix the test by replacing the string type by text type in the string.

--- elasticsearch-core/src/test/scala/com/sumologic/elasticsearch/restlastic/RestlasticSearchClientTest.scala
+++ elasticsearch-core/src/test/scala/com/sumologic/elasticsearch/restlastic/RestlasticSearchClientTest.scala
@@ -94,7 +94,8 @@ class RestlasticSearchClientTest extends WordSpec with Matchers with BeforeAndAf
       val mappingFut = restClient.putMapping(index, tpe, metadataMapping)
       whenReady(mappingFut) { _ => refresh() }
       val mappingRes = restClient.getMapping(index, tpe)
-      mappingRes.futureValue.jsonStr.toString.contains(""""f2":{"type":"string","index_options":"offsets","analyzer":"keyword_lowercase"}""") should be(true)
+
+      mappingRes.futureValue.jsonStr.toString.contains(""""f2":{"type":"text","index_options":"offsets","analyzer":"keyword_lowercase"}""") should be(true)
     }
 
     "Be able to create an index, index a document, and search it" in {
