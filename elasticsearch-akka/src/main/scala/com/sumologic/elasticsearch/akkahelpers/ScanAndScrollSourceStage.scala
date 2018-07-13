8b22442e5cf479a5f284296a9b42ce99eb75cf0e
Aditya Sule <asule@sumologic.com>
2018-07-13 12:36:06 -0700

    Licenses

--- elasticsearch-akka/src/main/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStage.scala
+++ elasticsearch-akka/src/main/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStage.scala
@@ -16,7 +16,6 @@
   * specific language governing permissions and limitations
   * under the License.
   */
-
 package com.sumologic.elasticsearch.akkahelpers
 
 import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
