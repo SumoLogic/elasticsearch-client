5804945c4a541d841530269903334b533687c767
Aditya Sule <asule@sumologic.com>
2018-07-13 11:29:52 -0700

    Quick implementation of Scan and scroll source using graphstage api

--- elasticsearch-akka/src/main/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStage.scala
+++ elasticsearch-akka/src/main/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStage.scala
@@ -1,5 +1,58 @@
 package com.sumologic.elasticsearch.akkahelpers
 
-class ScanAndScrollSourceStage {
+import akka.stream.{Attributes, Outlet, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
+import com.sumologic.elasticsearch.restlastic.RestlasticSearchClient.ReturnTypes
+import com.sumologic.elasticsearch.restlastic.RestlasticSearchClient.ReturnTypes.SearchResponse
+import com.sumologic.elasticsearch.restlastic.dsl.Dsl._
+import com.sumologic.elasticsearch.restlastic.ScrollClient
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class ScanAndScrollSourceStage(name: String = "UnnamedScanAndScrollSourceStage",
+                               index: Index,
+                               tpe: Type,
+                               query: QueryRoot,
+                               scrollSource: ScrollClient,
+                               sizeOpt: Option[Int],
+                               timeout: Duration = 10.seconds)
+  extends GraphStage[SourceShape[SearchResponse]] {
+
+  val outlet: Outlet[SearchResponse] = Outlet[SearchResponse](s"$name.out")
+
+  override val shape: SourceShape[SearchResponse] = SourceShape.of(outlet)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) with OutHandler {
+
+      private var resultFuture: Future[(ReturnTypes.ScrollId, SearchResponse)] = _
+
+      // TODO: Add some failure management that prints log messages, or throws sensible exceptions
+      private def next(): SearchResponse = {
+        val result = Await.result(resultFuture, timeout)
+        result match {
+          case (id, data) =>
+            resultFuture = scrollSource.scroll(id)
+            data
+        }
+      }
+
+      override def preStart(): Unit = {
+        resultFuture = scrollSource.startScrollRequest(index, tpe, query, sizeOpt = sizeOpt)
+      }
+
+      override def onPull(): Unit = {
+        val elem = next()
+
+        if (elem.length == 0) completeStage()
+
+        if (isAvailable(outlet)) {
+          push(outlet, elem)
+        }
+      }
+
+      setHandler(outlet, this)
+    }
 
 }
--- elasticsearch-akka/src/test/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStageTest.scala
+++ elasticsearch-akka/src/test/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceStageTest.scala
@@ -1,5 +1,85 @@
 package com.sumologic.elasticsearch.akkahelpers
 
-class ScanAndScrollSourceStageTest {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import com.sumologic.elasticsearch.restlastic.RestlasticSearchClient.ReturnTypes._
+import com.sumologic.elasticsearch.restlastic.ScrollClient
+import com.sumologic.elasticsearch.restlastic.dsl.Dsl
+import com.sumologic.elasticsearch.restlastic.dsl.Dsl._
+import org.json4s.Extraction._
+import org.json4s._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpec}
 
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class ScanAndScrollSourceStageTest extends WordSpec with Matchers with ScalaFutures {
+  val resultMaps: List[Map[String, AnyRef]] = List(Map("a" -> "1"), Map("a" -> "2"), Map("a" -> "3"))
+  implicit val formats = org.json4s.DefaultFormats
+  implicit val system = ActorSystem("test")
+  implicit val materializer = ActorMaterializer()
+
+  def searchResponseFromMap(map: Map[String, AnyRef]) = {
+    val raw = RawSearchResponse(Hits(List(ElasticJsonDocument("index", "type", "id", Some(0.1f), decompose(map).asInstanceOf[JObject], highlight = None)), 1))
+    SearchResponse(raw, "{}")
+  }
+
+  "ScanAndScrollSource" should {
+    val index = Index("index")
+    val tpe = Type("tpe")
+    val queryRoot = new QueryRoot(MatchAll)
+
+    "Read to the end of a source" in {
+      val searchResponses = resultMaps.map(searchResponseFromMap)
+      val client = new MockScrollClient(searchResponses)
+      // val sourced = Source.actorPublisher[SearchResponse](ScanAndScrollSource.props(index, tpe, queryRoot, client, sizeOpt = Some(5)))
+      val source = Source.fromGraph(new ScanAndScrollSourceStage("TestSource", index, tpe, queryRoot, client, sizeOpt = Some(5)))
+      val fut = source
+        .map(_.sourceAsMap)
+        .grouped(10)
+        .runWith(Sink.head)
+      whenReady(fut) { resp =>
+        resp.flatten should be(resultMaps)
+      }
+    }
+  }
+}
+
+
+class MockScrollClient(results: List[SearchResponse]) extends ScrollClient {
+  var id = 1
+  var started = false
+  var resultsQueue = results
+  override val indexExecutionCtx: ExecutionContext = ExecutionContext.Implicits.global
+  override def startScrollRequest(index: Dsl.Index, tpe: Dsl.Type, query: Dsl.QueryRoot,
+                                  resultWindowOpt: Option[String] = None, fromOpt: Option[Int] = None, sizeOpt: Option[Int] = None): Future[(ScrollId, SearchResponse)] = {
+    if (!started) {
+      started = true
+      processRequest()
+    } else {
+      Future.failed(new RuntimeException("Scroll already started"))
+    }
+  }
+
+  override def scroll(scrollId: ScrollId, resultWindowOpt: Option[String] = None): Future[(ScrollId, SearchResponse)] = {
+    if (scrollId.id.toInt == id) {
+      processRequest()
+    } else {
+      Future.failed(new RuntimeException("Invalid id"))
+    }
+
+  }
+
+  private def processRequest(): Future[(ScrollId, SearchResponse)] = {
+    id += 1
+    resultsQueue match {
+      case head :: rest =>
+        resultsQueue = rest
+        Future.successful((ScrollId(id.toString), head))
+      case Nil =>
+        Future.successful((ScrollId(id.toString), SearchResponse.empty))
+    }
+  }
 }
