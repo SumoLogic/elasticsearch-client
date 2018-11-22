e441c05759c448ec7e064f93214bf46753a7512c
Bartek Etenkowski <bartek@sumologic.com>
2018-11-22 01:16:18 +0100

    Issue-127: test fixed

--- elasticsearch-akka/src/test/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceTest.scala
+++ elasticsearch-akka/src/test/scala/com/sumologic/elasticsearch/akkahelpers/ScanAndScrollSourceTest.scala
@@ -70,7 +70,7 @@ class MockScrollClient(results: List[SearchResponse]) extends ScrollClient {
   var resultsQueue = results
   override val indexExecutionCtx: ExecutionContext = ExecutionContext.Implicits.global
   override def startScrollRequest(index: Dsl.Index, tpe: Dsl.Type, query: Dsl.QueryRoot,
-                                  resultWindowOpt: Option[String] = None, sizeOpt: Option[Int] = None): Future[(ScrollId, SearchResponse)] = {
+                                  resultWindowOpt: Option[String] = None, fromOpt: Option[Int] = None, sizeOpt: Option[Int] = None): Future[(ScrollId, SearchResponse)] = {
     if (!started) {
       started = true
       processRequest()
