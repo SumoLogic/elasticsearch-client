2ebe97e6ce2f8d4dee0295014802577a4a4c5b3d
Bartek Etenkowski <bartek@sumologic.com>
2018-12-10 15:19:21 +0100

    Issue-127: rep takes ES version into account

--- elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/MappingDsl.scala
+++ elasticsearch-core/src/main/scala/com/sumologic/elasticsearch/restlastic/dsl/MappingDsl.scala
@@ -86,15 +86,18 @@ trait MappingDsl extends DslCommons {
   }
 
   sealed trait IndexType {
-    val rep: String
+    def rep(version: EsVersion): String
   }
 
   case object NotAnalyzedIndex extends IndexType {
-    val rep = "not_analyzed"
+    override def rep(version: EsVersion): String = "not_analyzed"
   }
 
   case object NotIndexedIndex extends IndexType {
-    val rep = "no"
+    override def rep(version: EsVersion): String = version match {
+      case V2 => "no"
+      case V6 => "false"
+    }
   }
 
   case object MappingPath {
@@ -162,7 +165,7 @@ trait MappingDsl extends DslCommons {
 
     override def toJson(version: EsVersion): Map[String, Any] =
       Map(_type -> tpe.rep) ++
-        index.map(_index -> _.rep) ++
+        index.map(_index -> _.rep(version)) ++
         analyzer.map(_analyzer -> _.name) ++
         search_analyzer.map(_searchAnalyzer -> _.name) ++
         indexOption.map(_fieldIndexOpions -> _.option) ++
